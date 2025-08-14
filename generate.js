c351307437064ff97a93341bd523c005b3b38a3e
Vinyl <62996220+vincedprime@users.noreply.github.com>
2025-08-14 18:54:27 +0530

    Delete generate.js

--- generate.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const lines = fs.readFileSync('pickup-lines.txt', 'utf-8').split('\n').filter(Boolean);
-const randomLine = lines[Math.floor(Math.random() * lines.length)];
-
-fs.writeFileSync('data.json', JSON.stringify({ pickup_line: randomLine }, null, 2));
-console.log("Generated pickup line:", randomLine);
